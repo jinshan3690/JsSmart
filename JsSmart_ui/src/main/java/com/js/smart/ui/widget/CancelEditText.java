d4e1d17e5b2b2e6b3ae3f43fba5864c5360347cb
Js <190261036@qq.com>
2019-07-14 14:35:30 +0800

    第十七次功能完善

--- JsSmart_ui/src/main/java/com/js/smart/ui/widget/CancelEditText.java
+++ JsSmart_ui/src/main/java/com/js/smart/ui/widget/CancelEditText.java
@@ -83,11 +83,11 @@ public class CancelEditText extends LinearLayout implements OnFocusChangeListene
 
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CancelEditText);
 
-        cancelIcoWidth = (int) typedArray.getDimension(R.styleable.CancelEditText_cet_cancelIcoWidth, DensityUtil.dp2px(context, 15));
+        cancelIcoWidth = (int) typedArray.getDimension(R.styleable.CancelEditText_cet_cancelIcoWidth, LayoutParams.WRAP_CONTENT);
         cancelIcoHeight = (int) typedArray.getDimension(R.styleable.CancelEditText_cet_cancelIcoHeight, DensityUtil.dp2px(context, 15));
-        downIcoWidth = (int) typedArray.getDimension(R.styleable.CancelEditText_cet_downIcoWidth, DensityUtil.dp2px(context, 15));
+        downIcoWidth = (int) typedArray.getDimension(R.styleable.CancelEditText_cet_downIcoWidth, LayoutParams.WRAP_CONTENT);
         downIcoHeight = (int) typedArray.getDimension(R.styleable.CancelEditText_cet_downIcoHeight, DensityUtil.dp2px(context, 15));
-        switchIcoWidth = (int) typedArray.getDimension(R.styleable.CancelEditText_cet_switchIcoWidth, DensityUtil.dp2px(context, 22));
+        switchIcoWidth = (int) typedArray.getDimension(R.styleable.CancelEditText_cet_switchIcoWidth, LayoutParams.WRAP_CONTENT);
         switchIcoHeight = (int) typedArray.getDimension(R.styleable.CancelEditText_cet_switchIcoHeight, DensityUtil.dp2px(context, 22));
 
         initView(context);
